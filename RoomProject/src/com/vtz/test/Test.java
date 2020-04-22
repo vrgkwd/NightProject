7d4ac26d237022c1bc15d629f07f7efb2a5044c5
User <User@User-PC>
2020-04-23 01:32:21 +0530

    initial comments from STS new

---
+++ RoomProject/src/com/vtz/test/Test.java
@@ -0,0 +1,11 @@
+package com.vtz.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Utility u= new Utility();
+		System.out.println(u.getDate());
+	}
+
+}
---
+++ RoomProject/src/com/vtz/test/Utility.java
@@ -0,0 +1,10 @@
+package com.vtz.test;
+
+import java.util.Date;
+
+public class Utility {
+
+	 public Date getDate() {
+		 return new Date();
+	 }
+}
